794780a647709411d43ee7bf4d8d32d8858ee12e
Semyon Lukin <lukinsemyon@yandex.ru>
2024-02-01 18:41:45 +0500

    more fixtures added

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,13 @@
 """
 import pytest
 
-from formatters.models import BookModel, InternetResourceModel, ArticlesCollectionModel
+from formatters.models import (
+    BookModel,
+    InternetResourceModel,
+    ArticlesCollectionModel,
+    NormativeActModel,
+    JournalArticleModel,
+)
 
 
 @pytest.fixture
@@ -57,4 +63,43 @@ def articles_collection_model_fixture() -> ArticlesCollectionModel:
         publishing_house="АСТ",
         year=2020,
         pages="25-30",
+    )
+
+
+@pytest.fixture
+def normative_act_model_fixture() -> NormativeActModel:
+    """
+    Фикстура модели нормативного акта.
+
+    :return: ArticlesCollectionModel
+    """
+
+    return NormativeActModel(
+        type="Федеральный закон Российской Федерации",
+        title="О персональных данных",
+        adoption_date="27.07.2006",
+        act_numer="152-ФЗ",
+        source="Собрание законодательства Российской Федерации",
+        publication_year=2006,
+        source_number=29,
+        article_number=5233,
+        revision="06.02.2023",
+    )
+
+
+@pytest.fixture
+def journal_article_model_fixture() -> JournalArticleModel:
+    """
+    Фикстура модели статьи из журнала.
+
+    :return: ArticlesCollectionModel
+    """
+
+    return JournalArticleModel(
+        authors="Анохин Н. В., Протас Н. Г., Шмаков Е. К.",
+        article_title="ИИС – шаг вперед, дающий рывок в будущее",
+        journal_name="Идеи и идеалы",
+        publication_year=2021,
+        journal_number=3,
+        pages="266-280",
     )
