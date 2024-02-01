e2d809cc95329ef575a14173aba35d7e68f90697
Мария Плетнева <=>
2024-02-01 21:54:49 +0500

    added test fixtures for new sources

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,7 @@
 """
 import pytest
 
-from formatters.models import BookModel, InternetResourceModel, ArticlesCollectionModel
+from formatters.models import BookModel, InternetResourceModel, ArticlesCollectionModel, RegulatoryActModel, ArticleModel
 
 
 @pytest.fixture
@@ -57,4 +57,42 @@ def articles_collection_model_fixture() -> ArticlesCollectionModel:
         publishing_house="АСТ",
         year=2020,
         pages="25-30",
+    )
+
+
+@pytest.fixture
+def articles_collection_model_fixture() -> ArticleModel:
+    """
+    Фикстура модели статьи из журнала.
+
+    :return: ArticleModel
+    """
+
+    return ArticleModel(
+        authors="Иванов И.М., Петров С.Н.",
+        article_title="Наука как искусство",
+        journal_name="Сборник научных трудов",
+        No=2020,
+        year=2020,
+        pages="25-30",
+    )
+
+@pytest.fixture
+def articles_collection_model_fixture() -> RegulatoryActModel:
+    """
+    Фикстура модели нормативного актв.
+
+    :return: ArticleModel
+    """
+
+    return RegulatoryActModel(
+        act_type="Конституция Российской Федерации",
+        full_name="Наука как искусство",
+        acception_date="01.01.2000",
+        act_No="1234-56",
+        publishing_source="Парламентская газета",
+        year=2020,
+        source_No=5,
+        article_No=15,
+        amended_from="11.09.2002"
     )
