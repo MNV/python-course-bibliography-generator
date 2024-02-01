cec41ec9b7661b504e87475ee32743f54a668945
Semyon Lukin <lukinsemyon@yandex.ru>
2024-02-01 21:25:03 +0500

    tests for gost

--- src/tests/formatters/test_gost.py
+++ src/tests/formatters/test_gost.py
@@ -1,10 +1,21 @@
 """
 Тестирование функций оформления списка источников по ГОСТ Р 7.0.5-2008.
 """
-
 from formatters.base import BaseCitationFormatter
-from formatters.models import BookModel, InternetResourceModel, ArticlesCollectionModel
-from formatters.styles.gost import GOSTBook, GOSTInternetResource, GOSTCollectionArticle
+from formatters.models import (
+    BookModel,
+    InternetResourceModel,
+    ArticlesCollectionModel,
+    NormativeActModel,
+    JournalArticleModel,
+)
+from formatters.styles.gost import (
+    GOSTBook,
+    GOSTInternetResource,
+    GOSTCollectionArticle,
+    GOSTNormativeAct,
+    GOSTJournalArticle,
+)
 
 
 class TestGOST:
@@ -61,11 +72,49 @@ class TestGOST:
             == "Иванов И.М., Петров С.Н. Наука как искусство // Сборник научных трудов. – СПб.: АСТ, 2020. – С. 25-30."
         )
 
+    def test_journal_article(
+        self, journal_article_model_fixture: JournalArticleModel
+    ) -> None:
+        """
+        Тестирование форматирования сборника статей.
+
+        :param JournalArticleModel journal_article_model_fixture: Фикстура модели статьи из журнала
+        :return:
+        """
+
+        model = GOSTJournalArticle(journal_article_model_fixture)
+
+        assert (
+            model.formatted
+            == "Анохин Н. В., Протас Н. Г., Шмаков Е. К. ИИС – шаг вперед, дающий рывок в "
+            "будущее // Идеи и идеалы. 2021. № 3, С. 266-280."
+        )
+
+    def test_normative_act(
+        self, normative_act_model_fixture: NormativeActModel
+    ) -> None:
+        """
+        Тестирование форматирования сборника статей.
+
+        :param NormativeActModel normative_act_model_fixture: Фикстура модели статьи из журнала
+        :return:
+        """
+
+        model = GOSTNormativeAct(normative_act_model_fixture)
+
+        assert (
+            model.formatted
+            == "О персональных данных : Федеральный закон Российской Федерации от 27.07.2006 "
+            "№ 152-ФЗ // Собрание законодательства Российской Федерации. 2006. №29. Ст. 5233. в ред. от 06.02.2023."
+        )
+
     def test_citation_formatter(
         self,
         book_model_fixture: BookModel,
         internet_resource_model_fixture: InternetResourceModel,
         articles_collection_model_fixture: ArticlesCollectionModel,
+        normative_act_model_fixture: NormativeActModel,
+        journal_article_model_fixture: JournalArticleModel,
     ) -> None:
         """
         Тестирование функции итогового форматирования списка источников.
@@ -73,6 +122,8 @@ class TestGOST:
         :param BookModel book_model_fixture: Фикстура модели книги
         :param InternetResourceModel internet_resource_model_fixture: Фикстура модели интернет-ресурса
         :param ArticlesCollectionModel articles_collection_model_fixture: Фикстура модели сборника статей
+        :param NormativeActModel normative_act_model_fixture: Фикстура модели нормативного акта
+        :param JournalArticleModel journal_article_model_fixture: Фикстура модели статьи журнала
         :return:
         """
 
@@ -80,10 +131,13 @@ class TestGOST:
             GOSTBook(book_model_fixture),
             GOSTInternetResource(internet_resource_model_fixture),
             GOSTCollectionArticle(articles_collection_model_fixture),
+            GOSTNormativeAct(normative_act_model_fixture),
+            GOSTJournalArticle(journal_article_model_fixture),
         ]
         result = BaseCitationFormatter(models).format()
-
         # тестирование сортировки списка источников
-        assert result[0] == models[2]
-        assert result[1] == models[0]
-        assert result[2] == models[1]
+        assert result[0] == models[4]
+        assert result[1] == models[2]
+        assert result[2] == models[0]
+        assert result[3] == models[1]
+        assert result[4] == models[3]
